72d53b26635c155288da177667882812cedad592
Vitor Fernandes <vitorfernandes0763@gmail.com>
2026-05-17 18:33:27 -0300

    criando novas dags

---
+++ dags/12.rundag1.py
@@ -0,0 +1,23 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.providers.standart.operators.bash import BashOperator
+from airflow.providers.standart.operators.trigger_dagrun import TriggerDagRunOperator
+
+with DAG (
+    dag_id="dagrun1",
+    description="Dagrun1",
+    schedule=None,
+    start_date=datetime(2026, 5, 17),
+    catchup=False
+) as dag:
+    
+    task1 = BashOperator(task_id="tsk1", bash_command="sleep 5")
+    task2 = TriggerDagRunOperator(
+        task_id="tsk2",
+        trigger_dag_id="dagrundag2",
+        conf={"Chave": "Valor"},
+        wait_for_completion=True,
+        poke_interval=5
+    )
+
+    task1 >> task2
---
+++ dags/13.rundag2.py
@@ -0,0 +1,17 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG (
+    dag_id="dagrun2",
+    description="Dagrun2",
+    schedule=None,
+    start_date=datetime(2026, 5, 17),
+    catchup=False
+) as dag:
+    
+    task1 = BashOperator(task_id="tsk1", bash_command='echo "{{ dag_run.conf["Chave"] }}"')
+
+    task2 = BashOperator(task_id="tsk2", bash_command='sleep 5')
+
+    task1 >> task2
